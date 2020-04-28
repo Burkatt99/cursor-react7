bb2de80871bed89f401343142d383d10a0309ac2
Таня Бура <tanya.bura.99@gmail.com>
2020-04-28 19:55:13 +0300

    Updates

---
+++ precache-manifest.ef096e6882e1becc4c8e046a7d62d9b8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab5f0a32f32fc756326cf97ba976d9b",
+    "url": "/cursor-react7/index.html"
+  },
+  {
+    "revision": "d4aac4ab95cc17c4bc14",
+    "url": "/cursor-react7/static/css/main.5205ea28.chunk.css"
+  },
+  {
+    "revision": "43425a10d1131a980797",
+    "url": "/cursor-react7/static/js/2.eeee2955.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cursor-react7/static/js/2.eeee2955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4aac4ab95cc17c4bc14",
+    "url": "/cursor-react7/static/js/main.91b6cf68.chunk.js"
+  },
+  {
+    "revision": "382e2090084b587ee526",
+    "url": "/cursor-react7/static/js/runtime-main.0c269fe5.js"
+  },
+  {
+    "revision": "8f0cdaba773a67d96799d85e0515b8be",
+    "url": "/cursor-react7/static/media/author-min.8f0cdaba.jpg"
+  },
+  {
+    "revision": "67a0d5de29647e610f513ba46e0eb2e3",
+    "url": "/cursor-react7/static/media/books-min.67a0d5de.jpg"
+  },
+  {
+    "revision": "9251733ad0ff0cf5ab0b194c00859372",
+    "url": "/cursor-react7/static/media/dog.9251733a.png"
+  },
+  {
+    "revision": "8e1199e9ce41a9d7741c3989cee4a5ac",
+    "url": "/cursor-react7/static/media/fog.8e1199e9.jpg"
+  },
+  {
+    "revision": "b5eadc9c318d04eaf41a56ce859cb6fc",
+    "url": "/cursor-react7/static/media/pazzle-min.b5eadc9c.jpg"
+  },
+  {
+    "revision": "529ce1785b208896c6e3a12737c10fe9",
+    "url": "/cursor-react7/static/media/po.529ce178.png"
+  }
+]);
